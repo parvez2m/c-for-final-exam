1f5ad5ce00508bb33c6465275e5b40ddb1798aca
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-12-14 19:18:56 +0600

    add

--- exam.c
+++ exam.c
@@ -1,19 +1,30 @@
-#include<stdio.h>
+// #include<stdio.h>
  
-void main()
-{
-    int i, num1, num2, count = 0, sum = 0;
+// void main()
+// {
+//     int i, num1, num2, count = 0, sum = 0;
  
-    printf("Enter the value of num1 and num2 \n");
-    scanf("%d %d", &num1, &num2);
-    printf("Integers divisible by 5 are \n");
-    for (i = num1; i <= num2; i++)
-    {
-        if (i % 5 == 0)
-        {
-            printf("%d,", i);
+//     printf("Enter the value of num1 and num2 \n");
+//     scanf("%d %d", &num1, &num2);
+//     printf("Integers divisible by 5 are \n");
+//     for (i = num1; i <= num2; i++)
+//     {
+//         if (i % 5 == 0)
+//         {
+//             printf("%d,", i);
             
           
-        }
-    }
+//         }
+//     }
+// }
+
+
+
+#include<stdio.h>
+int main(){
+    int num[5] ={10,20,30,40,50};
+    int *pointer = num+1;
+    printf("pointer is %d\n",*pointer);
+    printf("The array %d ",*num);
+    return 0;
 }
--- finalsnote.c
+++ finalsnote.c
@@ -104,19 +104,19 @@
 ///**************Arrays****************************
 //Easy Fibonacci
 
-int main()
-{
-    int j, n, i, ar[1000];
-    scanf("%d", &n);
-    ar[0] = 0;
-    ar[1] = 1;
-    for(i = 2;i < n;i++){
-        ar[i] = ar[i-2] + ar[i-1];
-    }
-    //printf("0");
-    for(j = 0;j < n;j++){
-        printf(" %d", ar[j]);
-    }
-    printf("\n");
-    return 0;
-}
+// int main()
+// {
+//     int j, n, i, ar[1000];
+//     scanf("%d", &n);
+//     ar[0] = 0;
+//     ar[1] = 1;
+//     for(i = 2;i < n;i++){
+//         ar[i] = ar[i-2] + ar[i-1];
+//     }
+//     //printf("0");
+//     for(j = 0;j < n;j++){
+//         printf(" %d", ar[j]);
+//     }
+//     printf("\n");
+//     return 0;
+// }
